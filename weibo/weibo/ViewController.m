04bfc6640cdf4f277467b1fe90a601335d1ca4aa
dx11 <dx1111111>
2016-06-28 13:24:37 +0800

    1111

--- weibo/weibo/ViewController.m
+++ weibo/weibo/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"开始开发");
+    NSLog(@"11111111");
 }
 
 - (void)didReceiveMemoryWarning {
